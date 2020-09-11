fa0a6c96638c131502920f48b3aaf9a1e1ae296f
Exilic <tobias.engberg@exilic.se>
2020-09-11 09:10:01 +0200

    The final stage for now

--- Program.cs
+++ Program.cs
@@ -5,176 +5,174 @@ namespace CalculatorRPN
     class Program
     {
 
-        /*
-
-        Try to use Console.ReadKey();
-        How to do a "Last" function?   
-        What's the simplest way to handle too few arguments error?
-
-        */
 
         static void Main(string[] args) // This was here from the beginning and the program works, so they are left. Because why break something by acting while still ignorant?
 
         {
             int stackDepth = 0; // Haven't figured out how to intitialize this counter when the first actual instance happens
-            double[] theStack = new double[] { 0, 0, 1, 2, 3, 4, 5, 6, 7, 0, 0, 0 }; // Haven't figured out how to work with a dynamic stack of numbers, with no limit to how many numbers the user can enter and that could be initialized when numbers are entered.
+            double[] theStack = new double[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // Haven't figured out how to work with a dynamic stack of numbers, with no limit to how many numbers the user can enter and that could be initialized when numbers are entered.
             bool continueEntering = true;
 
-            Console.Clear(); // Problem here – doesn't manage to get this to clear the window fully from previous entries
-            Console.WriteLine("10:\n9:\n8:\n7:\n6:\n5:\n4:\n3:\n2:\n1:");
-
+           
             do
             {
-
-                var operands = GetOperands(theStack, stackDepth); // This line caused problems when the stack was empty – two extra items were added to the array "theStack", below line 1
-                
-                string userEntry = Console.ReadLine();
-
-                switch (userEntry)
-                {
-                    case "+":
-                        theStack[stackDepth] = MathTwoOperands(operands.number1, operands.number2, 1, stackDepth);
-                        break;
-
-                    case "/":
-                        theStack[stackDepth] = MathTwoOperands(operands.number1, operands.number2, 2, stackDepth);
-                        break;
-
-                    case "*":
-                        theStack[stackDepth] = MathTwoOperands(operands.number1, operands.number2, 3, stackDepth);
-                        break;
-
-                    case "-":
-                        theStack[stackDepth] = MathTwoOperands(operands.number1, operands.number2, 4, stackDepth);
-                        break;
-
-                    case "drop":
-                        stackDepth--;
-                        break;
-
-                    case "swap":
-                        double tempValue = theStack[stackDepth + 1];
-                        theStack[stackDepth + 1] = theStack[stackDepth];
-                        theStack[stackDepth] = tempValue;
-                        break;
-
-                    default:
-                        try
-                        {
-                            double numberEntry = Double.Parse(userEntry);
-                            theStack[stackDepth] = numberEntry;
-                            stackDepth++;
-                            Console.Clear();
-                            WriteLineNumbers(theStack, stackDepth);
-
-                        }
-                        catch (FormatException)
-                        {
-                            continueEntering = false;
-                        }
-                        break;
-                }
-
+                Console.Clear();
                 WriteLineNumbers(theStack, stackDepth);
 
-            } while (continueEntering);
+                string userEntry = Console.ReadLine();
 
+                bool twoOperandCommands = userEntry.Contains("+") || userEntry.Contains("-") || userEntry.Contains("/") || userEntry.Contains("*") || userEntry.Contains("swap");
+                bool oneOperandCommands = userEntry.Contains("drop") || userEntry.Contains("root") || userEntry.Contains("sqr") || userEntry.Contains("sin") || userEntry.Contains("cos") || userEntry.Contains("tan") || String.IsNullOrEmpty(userEntry);
 
-
-            /*
-        
-                if (stackDepth > 9) // theStack[] array is limited to 10 elements
+                if (stackDepth < 2 && twoOperandCommands)
                 {
-                    LimitReached();
+                    Console.WriteLine("Error – Too Few Arguments");
+                    System.Threading.Thread.Sleep(2300);
                 }
-            } while (continueEntering);
-
-        }
-        */
-
-            static void WriteLineNumbers(double[] value, int postsEntered)
-        {
-            for (int i = 10; i > postsEntered; i--)
-            {
-                Console.WriteLine(i + ":");
-            }
-            for (int i = postsEntered; i>0; i--)
-            {
-                Console.WriteLine(i + ":   " + value[postsEntered - i]); // Oldest entry should go highest in the console list, therefore the "postsEntered - 1"
-            }
-        }
-            /*
-        static void LimitReached()
-        {
-            string userEntry = Console.ReadLine();
-            Console.WriteLine(userEntry);
-        }
-
-        */
-
-        static double Multiplication(double number1, double number2)
-        {
-            double product = number1 * number2;
-            return product;
-        }
-
-        static double Addition(double number1, double number2)
-        {
-            double sum = number1 + number2;
-            return sum;
-        }
-
-        static double Subtraction(double number1, double number2)
-        {
-            double difference = number1 - number2;
-            return difference;
-        }
-
-        static double Division(double number1, double number2)
-        {
-            double quotient = number1 / number2;
-            return quotient;
-        }
-
-        static (double number1, double number2) GetOperands(double[] numberList, int postsEntered) // Googled on return of multiple values and ended up here: https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/builtin-types/value-tuples
-         {
-            double number1 = numberList[postsEntered + 1];
-            double number2 = numberList[postsEntered];
-            return (number1, number2);
-         }
-
-        static double MathTwoOperands(double num1, double num2, int kind, int postsEntered)
-            {
-                if (postsEntered > 1)
+                else if (stackDepth < 1 && oneOperandCommands)
                 {
-                    postsEntered--;
-                    switch (kind)
+                    Console.WriteLine("Error – Too Few Arguments");
+                    System.Threading.Thread.Sleep(2300);
+                }
+                else
+                {
+
+                    switch (userEntry)
                     {
-                        case 1:
-                            double outcome = Addition(num1, num2);
+                        case "+":
+                            theStack[stackDepth - 2] = Addition(theStack[stackDepth - 2], theStack[stackDepth - 1]);
+                            stackDepth--;
                             break;
 
-                        case 2:
-                            double outcome = Division(num1, num2);
+                        case "/":
+                            theStack[stackDepth - 2] = Division(theStack[stackDepth - 2], theStack[stackDepth - 1]);
+                            stackDepth--;
                             break;
 
-                        case 3:
-                            double outcome = Multiplication(num1, num2);
+                        case "*":
+                            theStack[stackDepth - 2] = Multiplication(theStack[stackDepth - 2], theStack[stackDepth - 1]);
+                            stackDepth--;
                             break;
 
-                        case 4:
-                            double outcome = Subtraction(num1, num2);
+                        case "-":
+                            theStack[stackDepth - 2] = Subtraction(theStack[stackDepth - 2], theStack[stackDepth - 1]);
+                            stackDepth--;
+                            break;
+
+                        case "sqr":
+                            theStack[stackDepth - 1] = Math.Pow(theStack[stackDepth - 1], 2);
+                            break;
+
+                        case "root":
+                            theStack[stackDepth - 1] = Math.Sqrt(theStack[stackDepth - 1]);
+                            break;
+
+                        case "sin":
+                            theStack[stackDepth - 1] = Math.Sin(theStack[stackDepth - 1]);
+                            break;
+
+                        case "cos":
+                            theStack[stackDepth - 1] = Math.Cos(theStack[stackDepth - 1]);
+                            break;
+
+                        case "tan":
+                            theStack[stackDepth - 1] = Math.Tan(theStack[stackDepth - 1]);
+                            break;
+
+                        case "drop":
+                            stackDepth--;
+                            break;
+
+                        case "swap":
+                            double tempValue = theStack[stackDepth - 1];
+                            theStack[stackDepth - 1] = theStack[stackDepth - 2];
+                            theStack[stackDepth - 2] = tempValue;
+                            break;
+
+                        case "":
+                            if (stackDepth > 9)
+                            {
+                                Console.WriteLine("Error – Only Ten Numbers Total");
+                                System.Threading.Thread.Sleep(2300);
+                            }   else  {
+                                theStack[stackDepth] = theStack[stackDepth - 1];
+                                stackDepth++;
+                            }
+                            break;
+
+                        case "?":
+                            Console.WriteLine("+  -  *  /  sqr  root  sin  cos  tan  drop  swap  quit\nPress any key to continue");
+                            Console.ReadKey();
+                            break;
+
+                        case "quit":
+                            continueEntering = false;
                             break;
 
                         default:
+
+                            if (stackDepth > 9)
+                            {
+                                Console.WriteLine("Error – Only Ten Numbers Total");
+                                System.Threading.Thread.Sleep(2300);
+                            }
+                            else
+                            {
+                                try
+                                {
+                                    theStack[stackDepth] = Double.Parse(userEntry);
+                                    stackDepth++;
+                                }
+                                catch (FormatException)
+                                {
+                                    Console.WriteLine("Error – Invalid Operation");
+                                    System.Threading.Thread.Sleep(2300);
+                                }
+                            }
                             break;
-                    } } else
+                    }
+                }
+                
+            } while (continueEntering);
+
+
+            static void WriteLineNumbers(double[] value, int postsEntered)
+            {
+                Console.WriteLine("For a list of actions, press ?");
+                for (int i = 10; i > postsEntered; i--)
                 {
-                    Console.WriteLine("Error – Too Few Arguments");
-                    double outcome = num1;
-                     }
-                        return outcome;
+                    Console.WriteLine(i + ":");
+                }
+                for (int i = postsEntered; i > 0; i--)
+                {
+                    Console.WriteLine(i + ":   " + value[postsEntered - i]); // Oldest entry should go highest in the console list, therefore the "postsEntered - 1"
+                }
+            }
+
+            static double Multiplication(double number1, double number2)
+            {
+                double product = number1 * number2;
+                return product;
+            }
+
+            static double Addition(double number1, double number2)
+            {
+                double sum = number1 + number2;
+                return sum;
+            }
+
+            static double Subtraction(double number1, double number2)
+            {
+                double difference = number1 - number2;
+                return difference;
+            }
+
+            static double Division(double number1, double number2)
+            {
+                double quotient = number1 / number2;
+                return quotient;
             }
         }
     }
 }
+ 
